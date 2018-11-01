161e42f8813809a2a1dc997f7d56d2cdf62d72cf
javier barboza vizcaino <jabavi8@hotmail.com>
2018-11-01 15:26:09 -0600

    commit javier

--- Sinem/Models/SinemDBContext.cs
+++ Sinem/Models/SinemDBContext.cs
@@ -10,7 +10,7 @@ namespace Sinem.Models
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class SinemDBContext : DbContext
     {
-        public SinemDBContext() : base("Server=localhost;Database=bd_sinem;Uid=root;Pwd=1234; port=3307")
+        public SinemDBContext() : base("Server=localhost;Database=bd_sinem;Uid=root;Pwd=1234;")
         {
             this.Configuration.LazyLoadingEnabled = false;
         }
@@ -20,8 +20,6 @@ namespace Sinem.Models
         public DbSet<Curso> Cursos { get; set; }
         public DbSet<Horario> Horarios { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
-
-        public DbSet<GestionCurso> GestionCursos { get; set;}
 
         public DbSet<Permiso>Permisos { get; set; }
         public DbSet<Rol> Roles { get; set; }
